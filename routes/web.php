2e00f300156d8ebcbfec8ba63bc9ea3e0ea03241
Taro Aoyama <pc@aoyamania.jp>
2023-08-13 20:04:53 +0900

    flatpickr

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ use App\Http\Controllers\EventController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::middleware([
     'auth:sanctum',
