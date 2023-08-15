6398d3fd33fadd27786c2e64dafbcb784f2e3b05
Taro Aoyama <pc@aoyamania.jp>
2023-08-15 13:25:52 +0900

    event past

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -19,7 +19,10 @@ class EventController extends Controller
      */
     public function index()
     {
+        $today = Carbon::today();
+
         $events = DB::table('events')
+            ->whereDate('start_date', '>=', $today)
             ->orderBy('start_date', 'asc')
             ->paginate(10);
 
@@ -36,28 +39,24 @@ class EventController extends Controller
         return view('manager.events.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreEventRequest  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(StoreEventRequest $request)
     {
 
         $check = EventService::checkEventDuplication(
-            $request['event_date'],$request['start_time'],$request['end_time']);
+            $request['event_date'], $request['start_time'], $request['end_time']
+        );
 
 
         // dd($check);
-        if($check){
+        if ($check) {
             session()->flash('status', 'この時間帯はすでに他の予約が存在しています。');
             return view('manager.events.create');
 
         }
 
-        $startDate = EventService::joinDateAndTime($request['event_date'],$request["start_time"]);
-        $endDate = EventService::joinDateAndTime($request['event_date'],$request["end_time"]);
+        $startDate = EventService::joinDateAndTime($request['event_date'], $request["start_time"]);
+        $endDate = EventService::joinDateAndTime($request['event_date'], $request["end_time"]);
 
         Event::create([
             'name' => $request['event_name'],
@@ -82,45 +81,76 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        $event = Event::findOrFail($event->id);//このEventはEventモデル
+        $event = Event::findOrFail($event->id); //このEventはEventモデル
         $eventDate = $event->eventDate;
         $eventTime = $event->startTime;
         $endTime = $event->endTime;
 
         // dd($eventDate,$eventTime,$endTime);
 
-        return view('manager.events.show',compact('event','eventDate','eventTime','endTime'));
+        return view('manager.events.show', compact('event', 'eventDate', 'eventTime', 'endTime'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Event  $event
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit(Event $event)
     {
-        //
+        $event = Event::findOrFail($event->id); //このEventはEventモデル
+        $eventDate = $event->editEventDate;
+        $startTime = $event->startTime;
+        $endTime = $event->endTime;
+
+
+        return view('manager.events.edit', compact('event', 'eventDate', 'startTime', 'endTime'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateEventRequest  $request
-     * @param  \App\Models\Event  $event
-     * @return \Illuminate\Http\Response
-     */
+
+
     public function update(UpdateEventRequest $request, Event $event)
     {
-        //
+        $check = EventService::countEventDuplication(
+            $request['event_date'], $request['start_time'], $request['end_time']
+        );
+
+
+        // dd($check);
+        if ($check > 1) {
+            $event = Event::findOrFail($event->id); //このEventはEventモデル
+            $eventDate = $event->editEventDate;
+            $startTime = $event->startTime;
+            $endTime = $event->endTime;
+            session()->flash('status', 'この時間帯はすでに他の予約が存在しています。');
+            return view('manager.events.edit', compact('event', 'eventDate', 'startTime', 'endTime'));
+
+        }
+
+        $startDate = EventService::joinDateAndTime($request['event_date'], $request["start_time"]);
+        $endDate = EventService::joinDateAndTime($request['event_date'], $request["end_time"]);
+
+        $event = Event::findOrFail($event->id); //このEventはEventモデル
+        $event->name = $request['event_name'];
+        $event->information = $request['information'];
+        $event->start_date = $startDate;
+        $event->end_date = $endDate;
+        $event->max_people = $request['max_people'];
+        $event->is_visible = $request['is_visible'];
+        $event->save();
+
+
+        session()->flash('status', '更新が完了しました。');
+
+        return to_route('events.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Event  $event
-     * @return \Illuminate\Http\Response
-     */
+
+    public function past(){
+        $today = Carbon::today();//今日の日付を取得
+        $events = DB::table('events')//eventsテーブルを取得
+        ->whereDate('start_date', '<', $today)//開始日が今日より前
+        ->orderBy('start_date' , 'desc')
+        ->paginate(10);
+
+        return view('manager.events.past', compact('events'));
+    }
     public function destroy(Event $event)
     {
         //
--- resources/views/manager/events/past.blade.php
+++ resources/views/manager/events/past.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            イベント管理
+            過去のイベント一覧
         </h2>
     </x-slot>
 
@@ -17,12 +17,7 @@
                             </div>
                         @endif
 
-                        <div class="flex justify-between">
-                            <button onclick="location.href='{{ route('events.past') }}'"
-                                class="flex mb-4 ml-auto text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">過去のイベント一覧</button>
-                            <button onclick="location.href='{{ route('events.create') }}'"
-                                class="flex mb-4 ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">新規登録</button>
-                        </div>
+
 
                         <div class="w-full mx-auto overflow-auto">
                             <table class="table-auto w-full text-left whitespace-no-wrap">
@@ -55,9 +50,7 @@
                                 <tbody>
                                     @foreach ($events as $event)
                                         <tr>
-                                            <td class="text-blue-500 px-4 py-3"><a
-                                                    href="{{ route('events.show', ['event' => $event->id]) }}">{{ $event->name }}</a>
-                                            </td>
+                                            <td class="text-blue-500 px-4 py-3"><a href="{{ route('events.show', ['event'=> $event->id]) }}">{{  $event->name  }}</a></td>
                                             <td class="px-4 py-3">{{ $event->start_date }}</td>
                                             <td class="px-4 py-3">{{ $event->end_date }}</td>
                                             <td class="px-4 py-3">after</td>
